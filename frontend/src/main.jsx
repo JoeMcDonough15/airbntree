c897f1d299c7dfb72642483406d66d1f2f899f7d
joemcdonough15 <joemcdonoughmusic@gmail.com>
2024-10-10 21:12:09 -0400

    connect redux store to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Provider } from "redux";
+import { Provider } from "react-redux";
 import configureStore from "./store";
 import "./index.css";
 
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,16 +1,16 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 
 const rootReducer = combineReducers({});
 
 let enhancer;
 
-if (!import.meta.env.MODE === "production") {
+if (!(import.meta.env.MODE === "production")) {
   // development
   const logger = (await import("redux-logger")).default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  enhancer = composeEnhancers(logger, thunk);
+  enhancer = composeEnhancers(applyMiddleware(logger, thunk));
 } else {
   // production
   enhancer = applyMiddleware(thunk);
